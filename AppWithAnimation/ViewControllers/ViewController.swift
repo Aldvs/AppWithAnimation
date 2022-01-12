9afcb68ab40236a241d565b5e2e943a42f4824d6
aldvs <andrewgogol777@gmail.com>
2022-01-12 18:13:56 +0300

    Add IBOutlet and IB Actions

--- AppWithAnimation/ViewControllers/ViewController.swift
+++ AppWithAnimation/ViewControllers/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coreAnimationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func runCoreAnimation(_ sender: UIButton) {
+    }
+    
 }
 
